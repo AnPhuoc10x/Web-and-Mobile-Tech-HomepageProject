25774183ec4619bee4d3236266b9cb5d5187045a
AnPhuoc10x <105622510+AnPhuoc10x@users.noreply.github.com>
2022-06-19 09:58:25 +0900

    Add files via upload

--- HomePage/script.js
+++ HomePage/script.js
@@ -66,3 +66,75 @@ function About_ButtonClick() {
 }
 HomePage_Button.addEventListener("click", HomePage_ButtonClick);
 About_Button.addEventListener("click", About_ButtonClick);
+
+
+//Ảnh chạy
+const PHOTO_LIST = [
+  "https://i.ibb.co/LhhsyhN/deepdish.jpg",
+  "https://i.ibb.co/fQdypjL/flatbread.jpg",
+  "https://i.ibb.co/bLJQz5L/frenchbread.jpg"
+];
+const PHOTO_NAME_LIST = ["Meat Lover Pizza","Olive Pizza","Cheese Pizza"];
+
+function createImage(src) {
+  const image = document.createElement("img");
+  image.src = src;
+  picture = document.querySelector("#ProductImage");
+  SlideImage = picture.appendChild(image);
+  SlideImage.classList.add(".SlideImage");
+}
+
+
+const ImageBox = document.querySelector("#ProductImage");
+const buttonLeft = document.querySelector(".ButtonLeft");
+const buttonRight = document.querySelector(".ButtonRight");
+const ProductName = document.querySelector(".ProductName");
+buttonLeft.addEventListener('click', LeftonClick); 
+buttonRight.addEventListener('click', RightonClick); 
+
+createImage(PHOTO_LIST[0]); //create image when page first load
+ProductName.innerHTML=PHOTO_NAME_LIST[0]; //create product name when page first load
+
+function ChangeProductName() {
+  
+  ProductName.innerHTML= ImageName;
+}
+
+let nextIndex = 0;
+function LeftonClick() {
+  if (nextIndex == 0) {
+    return;
+  }
+  else {
+  console.log("Left clicked");
+  nextIndex--;
+  console.log(nextIndex);
+  ImageBox.innerHTML = '';
+  const AppearImage = createImage(PHOTO_LIST[nextIndex]);
+     ImageName = PHOTO_NAME_LIST[nextIndex];
+    console.log(ImageName);
+    ChangeProductName();
+  //ImageBox.appendChild(AppearImage); 
+
+  }
+  
+  }
+
+function RightonClick(){
+   if (nextIndex == ((PHOTO_LIST.length) - 1)) {
+     return;
+   }
+  else {
+  console.log("Right clicked");
+  nextIndex++;
+  console.log(nextIndex);
+  ImageBox.innerHTML = '';
+  const AppearImage = createImage(PHOTO_LIST[nextIndex]);
+     ImageName = PHOTO_NAME_LIST[nextIndex];
+    console.log(ImageName);
+     ChangeProductName();
+ // ImageBox.appendChild(AppearImage); 
+   
+
+  }
+}
